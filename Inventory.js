5846fd108aff370f794cc2bafff13a2cd0f8e6e8
rmotati <50639001+rmotati@users.noreply.github.com>
2024-06-25 09:27:35 +0530

    policies count added - Update Inventory.js

--- Inventory.js
+++ Inventory.js
@@ -1,12 +1,15 @@
 // Inventory.js
 
 var _table_ = document.createElement('table'),
-_tr_ = document.createElement('tr'),
-_th_ = document.createElement('th'),
-_td_ = document.createElement('td');
+    _tr_ = document.createElement('tr'),
+    _th_ = document.createElement('th'),
+    _td_ = document.createElement('td');
 const dataTable = document.getElementById('dataTable');
+const policiesCountEl = document.getElementById('policiesCount');
+
 dataTable.innerHTML = '';
 var data;
+var policiesCount;
 var filteredData;
 var searchData;
 // var tableName = "checkov"
@@ -18,16 +21,16 @@ function getDatafromtabs(sideTabName) {
     var mainTabName = document.querySelector('.tab.active').getAttribute('data-table');
     const sideTabs = document.querySelectorAll('.side-tab');
     sideTabs.forEach(tab => {
-        tab.addEventListener('click', function () {        
+        tab.addEventListener('click', function () {
             sideTabs.forEach(tab => tab.classList.remove('active'));
             this.classList.add('active');
         });
     });
-    if(mainTabName === 'bugging') {
-        renderTableForBugging(data.bugging, sideTabName); 
+    if (mainTabName === 'bugging') {
+        renderTableForBugging(data.bugging, sideTabName);
     }
-    else if( mainTabName === 'enforce')  {
-        renderEnforceTable(data.enforced,sideTabName)
+    else if (mainTabName === 'enforce') {
+        renderEnforceTable(data.enforced, sideTabName)
     }
 }
 
@@ -37,13 +40,13 @@ function fetchyamldata(policystatus) {
     // document.querySelectorAll('.side-tab')[1].classList.remove('active')
     // document.querySelectorAll('.side-tab')[2].classList.remove('active')
 
-      const tabs = document.querySelectorAll('.tab');
-      tabs.forEach(tab => {
-          tab.addEventListener('click', function () {        
-              tabs.forEach(tab => tab.classList.remove('active'));
-              this.classList.add('active');
-          });
-        })
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            tabs.forEach(tab => tab.classList.remove('active'));
+            this.classList.add('active');
+        });
+    })
     if (policystatus) {
 
         var tabName = document.querySelector('.side-tab.active').getAttribute('data-table');
@@ -51,9 +54,9 @@ function fetchyamldata(policystatus) {
         fetch(policystatus)
             .then(response => response.text())
             .then(text => {
-              data = jsyaml.load(text);
-                    getDatafromtabs(tabName)
-               
+                data = jsyaml.load(text);
+                getDatafromtabs(tabName)
+
             })
             .catch(error => console.error('Error loading YAML file:', error));
     }
@@ -63,7 +66,8 @@ function fetchyamldata(policystatus) {
 function renderTableForBugging(data, sideTabName) {
     dataTable.innerHTML = "";
     if (sideTabName !== 'checkov' && sideTabName !== 'docker' && sideTabName !== 'helm') return; // Only render for specified tabs
-     filteredData = data.filter(item => item.namespace.includes(sideTabName));
+    filteredData = data.filter(item => item.namespace.includes(sideTabName));
+    policiesCount = filteredData.length;
     const table = document.createElement('table');
     table.innerHTML = ""
     const headerRow = table.insertRow();
@@ -86,20 +90,21 @@ function renderTableForBugging(data, sideTabName) {
     });
 
     dataTable.appendChild(table);
+    policiesCountEl.innerText = "Total Policies : " + policiesCount
 }
 
 
 function renderEnforceTable(data, sideTabName) {
     dataTable.innerHTML = "";
-    if(sideTabName === "checkov") {
-         filteredData = data.filter(item => item.namespace.includes(sideTabName));
-         searchData = data.filter(item => item.namespace.includes(sideTabName));
+    if (sideTabName === "checkov") {
+        filteredData = data.filter(item => item.namespace.includes(sideTabName));
+        searchData = data.filter(item => item.namespace.includes(sideTabName));
 
     }
     else {
-         filteredData = data.filter(item => item.namespace.includes(`release.${sideTabName}`));
+        filteredData = data.filter(item => item.namespace.includes(`release.${sideTabName}`));
     }
-
+    policiesCount = filteredData.length;
     const table = document.createElement('table');
     const headerRow = table.insertRow();
     headerRow.innerHTML = `
@@ -110,7 +115,7 @@ function renderEnforceTable(data, sideTabName) {
     `;
     filteredData.forEach((item, index) => {
         var exceptions;
-        if(item.exceptions) {
+        if (item.exceptions) {
             exceptions = item.exceptions.map(function (item) {
                 return item['service'];
             });
@@ -118,7 +123,7 @@ function renderEnforceTable(data, sideTabName) {
         else {
             exceptions = "";
         }
-      
+
         const row = table.insertRow();
         row.innerHTML = `
             <td>${index + 1}</td>
@@ -128,60 +133,77 @@ function renderEnforceTable(data, sideTabName) {
     });
 
     dataTable.appendChild(table);
-
-   
+    policiesCountEl.innerText = "Total Policies : " + policiesCount;
 }
 
 function getIncubatingData() {
     dataTable.innerHTML = "<h3 class='work-progress'>Work in progress</h3>";
+    policiesCountEl.innerText = " ";
 }
 
 
 const searchInput = document.getElementById('searchPolicy');
-searchInput.addEventListener('input',function() {
+searchInput.addEventListener('input', function () {
 
     var tabName = document.querySelector('.tab.active').getAttribute('data-table');
     var sideTabName = document.querySelector('.side-tab.active').getAttribute('data-table');
     const searchText = this.value.toLowerCase();
 
-    if(tabName === 'bugging') {
-        var buggingData =  data.bugging.filter(item => item.namespace.includes(sideTabName)).filter(item => item.namespace.includes(sideTabName))
+    if (tabName === 'bugging') {
+        var buggingData = data.bugging.filter(item => item.namespace.includes(sideTabName)).filter(item => item.namespace.includes(sideTabName))
         var filteredData = buggingData.filter(item => {
             return Object.values(item).some(val => String(val).toLowerCase().includes(searchText))
         })
-        renderTableForBugging(filteredData,sideTabName)
+        renderTableForBugging(filteredData, sideTabName)
     }
-    else if( tabName === 'enforce')  {
+    else if (tabName === 'enforce') {
         var enforcedData = data.enforced.filter(item => item.namespace.includes(sideTabName)).filter(item => item.namespace.includes(sideTabName));
-        var filteredData =  enforcedData.filter(item => {
+        var filteredData = enforcedData.filter(item => {
             return Object.values(item).some(val => String(val).toLowerCase().includes(searchText))
         })
-        renderEnforceTable(filteredData,sideTabName)
+        renderEnforceTable(filteredData, sideTabName)
     }
     else {
         var unEnforcedData = data.unenforced.filter(item => item.namespace.includes(sideTabName)).filter(item => item.namespace.includes(sideTabName));
-        var filteredData =  unEnforcedData.filter(item => {
+        var filteredData = unEnforcedData.filter(item => {
             return Object.values(item).some(val => String(val).toLowerCase().includes(searchText))
         })
-        renderEnforceTable(filteredData,sideTabName)
+        renderEnforceTable(filteredData, sideTabName)
     }
 
-   
+
 })
 
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.childNodes[1].classList.toggle("fa-caret-down")
-    this.childNodes[1].classList.toggle("fa-caret-right")
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
+    dropdown[i].addEventListener("click", function () {
+        this.childNodes[1].classList.toggle("fa-caret-down")
+        this.childNodes[1].classList.toggle("fa-caret-right")
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "none") {
+            dropdownContent.style.display = "block";
+        } else {
+            dropdownContent.style.display = "none";
+        }
+    });
 }
+
+var ppdropdown = document.getElementsByClassName("pp-dropdown-btn");
+var i;
+for (i = 0; i < ppdropdown.length; i++) {
+    ppdropdown[i].addEventListener("click", function () {
+        this.childNodes[1].classList.toggle("fa-caret-down")
+        this.childNodes[1].classList.toggle("fa-caret-right")
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
